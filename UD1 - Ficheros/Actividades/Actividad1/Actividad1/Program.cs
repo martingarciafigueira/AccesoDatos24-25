2f1650f0e0766f4586979debc447c7e8267e635a
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-12-03 18:26:28 +0100

    Actividad1

---
+++ UD1 - Ficheros/Actividades/Actividad1/Actividad1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Actividad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoIntroducido;
+            bool continuaInsertandoFrases = true;
+
+            using (StreamWriter fichero = new StreamWriter("Log_Fichero.txt"))
+            {
+                while (continuaInsertandoFrases)
+                {
+                    Console.Write("Introduce una frase, por favor: ");
+                    textoIntroducido = Console.ReadLine();
+
+                    if (textoIntroducido == "END")
+                    {
+                        continuaInsertandoFrases = false;
+                        break;
+                    }
+
+                    fichero.WriteLine(textoIntroducido);
+                }
+            }
+        }
+    }
+}

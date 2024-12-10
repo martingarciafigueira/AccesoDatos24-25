c459ff19d17366b5f7f7701f0172760e5628285e
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-12-10 17:56:27 +0100

    Actividad8

---
+++ UD1 - Ficheros/Actividades/Actividad8/Actividad7/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Actividad7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsertarDatos = new System.Windows.Forms.Button();
+            this.btnMostrarInfoFichero = new System.Windows.Forms.Button();
+            this.txtDatosFichero = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellidos = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInsertarDatos
+            // 
+            this.btnInsertarDatos.Location = new System.Drawing.Point(105, 191);
+            this.btnInsertarDatos.Name = "btnInsertarDatos";
+            this.btnInsertarDatos.Size = new System.Drawing.Size(118, 60);
+            this.btnInsertarDatos.TabIndex = 1;
+            this.btnInsertarDatos.Text = "Insertar datos";
+            this.btnInsertarDatos.UseVisualStyleBackColor = true;
+            this.btnInsertarDatos.Click += new System.EventHandler(this.btnInsertarDatos_Click);
+            // 
+            // btnMostrarInfoFichero
+            // 
+            this.btnMostrarInfoFichero.Location = new System.Drawing.Point(712, 239);
+            this.btnMostrarInfoFichero.Name = "btnMostrarInfoFichero";
+            this.btnMostrarInfoFichero.Size = new System.Drawing.Size(118, 60);
+            this.btnMostrarInfoFichero.TabIndex = 2;
+            this.btnMostrarInfoFichero.Text = "Ver datos";
+            this.btnMostrarInfoFichero.UseVisualStyleBackColor = true;
+            this.btnMostrarInfoFichero.Click += new System.EventHandler(this.btnMostrarInfoFichero_Click);
+            // 
+            // txtDatosFichero
+            // 
+            this.txtDatosFichero.Location = new System.Drawing.Point(632, 334);
+            this.txtDatosFichero.Multiline = true;
+            this.txtDatosFichero.Name = "txtDatosFichero";
+            this.txtDatosFichero.ReadOnly = true;
+            this.txtDatosFichero.Size = new System.Drawing.Size(282, 243);
+            this.txtDatosFichero.TabIndex = 3;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(63, 58);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellidos
+            // 
+            this.lblApellidos.AutoSize = true;
+            this.lblApellidos.Location = new System.Drawing.Point(63, 120);
+            this.lblApellidos.Name = "lblApellidos";
+            this.lblApellidos.Size = new System.Drawing.Size(64, 16);
+            this.lblApellidos.TabIndex = 5;
+            this.lblApellidos.Text = "Apellidos";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(163, 55);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(163, 114);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(100, 22);
+            this.txtApellidos.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1171, 624);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblApellidos);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtDatosFichero);
+            this.Controls.Add(this.btnMostrarInfoFichero);
+            this.Controls.Add(this.btnInsertarDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnInsertarDatos;
+        private System.Windows.Forms.Button btnMostrarInfoFichero;
+        private System.Windows.Forms.TextBox txtDatosFichero;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+    }
+}
+
---
+++ UD1 - Ficheros/Actividades/Actividad8/Actividad7/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertarDatos_Click(object sender, EventArgs e)
+        {
+            StreamWriter fichero;
+            string nombreCompleto = txtNombre.Text + " " + txtApellidos.Text;
+            
+            fichero = File.AppendText(@"Alumnos\Nombres.txt");
+            fichero.WriteLine(nombreCompleto);
+            fichero.Close();
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            mostrarInfo();
+        }
+
+        private void btnMostrarInfoFichero_Click(object sender, EventArgs e)
+        {
+            mostrarInfo();
+        }
+
+        public void mostrarInfo()
+        {
+            StreamReader fichero;
+            fichero = File.OpenText(@"Alumnos\Nombres.txt");
+            txtDatosFichero.Text = fichero.ReadToEnd();
+            fichero.Close();
+        }
+    }
+}

a2af291d8035631a75bc7a443f5239c1264fcc1a
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2025-01-21 18:45:39 +0100

    TallerFinal

---
+++ UD3 - Trabajando con SQL Server/Actividades/24-25/TallerFinal/Actividad4/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Actividad4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFutbolistas = new System.Windows.Forms.ListBox();
+            this.btnMostrarFutbolistas = new System.Windows.Forms.Button();
+            this.panelVisualizacion = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panelVisualizacion.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstFutbolistas
+            // 
+            this.lstFutbolistas.FormattingEnabled = true;
+            this.lstFutbolistas.ItemHeight = 16;
+            this.lstFutbolistas.Location = new System.Drawing.Point(28, 29);
+            this.lstFutbolistas.Name = "lstFutbolistas";
+            this.lstFutbolistas.Size = new System.Drawing.Size(253, 340);
+            this.lstFutbolistas.TabIndex = 0;
+            // 
+            // btnMostrarFutbolistas
+            // 
+            this.btnMostrarFutbolistas.Location = new System.Drawing.Point(71, 387);
+            this.btnMostrarFutbolistas.Name = "btnMostrarFutbolistas";
+            this.btnMostrarFutbolistas.Size = new System.Drawing.Size(139, 56);
+            this.btnMostrarFutbolistas.TabIndex = 1;
+            this.btnMostrarFutbolistas.Text = "Mostrar futbolistas";
+            this.btnMostrarFutbolistas.UseVisualStyleBackColor = true;
+            this.btnMostrarFutbolistas.Click += new System.EventHandler(this.btnMostrarFutbolistas_Click);
+            // 
+            // panelVisualizacion
+            // 
+            this.panelVisualizacion.Controls.Add(this.button5);
+            this.panelVisualizacion.Controls.Add(this.button4);
+            this.panelVisualizacion.Controls.Add(this.button3);
+            this.panelVisualizacion.Controls.Add(this.listBox1);
+            this.panelVisualizacion.Controls.Add(this.lstFutbolistas);
+            this.panelVisualizacion.Controls.Add(this.btnMostrarFutbolistas);
+            this.panelVisualizacion.Location = new System.Drawing.Point(12, 35);
+            this.panelVisualizacion.Name = "panelVisualizacion";
+            this.panelVisualizacion.Size = new System.Drawing.Size(647, 615);
+            this.panelVisualizacion.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(345, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 340);
+            this.listBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(744, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(686, 459);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Equipo";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(161, 242);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Insertar futbolista";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(461, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 41);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Insertar equipo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(404, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 32);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Estadio";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(515, 242);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(124, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(515, 172);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(124, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(404, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 32);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Codigo";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(515, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(124, 22);
+            this.textBox6.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(404, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 32);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Nombre";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(388, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 56);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mostrar equipos";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(71, 469);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Borrar futbolista";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(388, 469);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 56);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Borrar equipo";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 749);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelVisualizacion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelVisualizacion.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFutbolistas;
+        private System.Windows.Forms.Button btnMostrarFutbolistas;
+        private System.Windows.Forms.Panel panelVisualizacion;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+

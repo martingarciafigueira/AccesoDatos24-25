47321877a23a8e633e036471fe60c919f744e469
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2025-01-28 19:52:43 +0100

    Actividad3B

---
+++ UD4 - ORM/Actividades/Actividad3B/Actividad3/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Actividad3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstEquipos = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstFutbolistas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstEquipos
+            // 
+            this.lstEquipos.FormattingEnabled = true;
+            this.lstEquipos.ItemHeight = 16;
+            this.lstEquipos.Location = new System.Drawing.Point(176, 159);
+            this.lstEquipos.Name = "lstEquipos";
+            this.lstEquipos.Size = new System.Drawing.Size(300, 308);
+            this.lstEquipos.TabIndex = 1;
+            this.lstEquipos.DoubleClick += new System.EventHandler(this.lstEquipos_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(261, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Equipos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(809, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Futbolistas";
+            // 
+            // lstFutbolistas
+            // 
+            this.lstFutbolistas.FormattingEnabled = true;
+            this.lstFutbolistas.ItemHeight = 16;
+            this.lstFutbolistas.Location = new System.Drawing.Point(724, 159);
+            this.lstFutbolistas.Name = "lstFutbolistas";
+            this.lstFutbolistas.Size = new System.Drawing.Size(300, 308);
+            this.lstFutbolistas.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1212, 651);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstFutbolistas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstEquipos);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstEquipos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstFutbolistas;
+    }
+}
+
---
+++ UD4 - ORM/Actividades/Actividad3B/Actividad3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Actividad3.Clases;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad3
+{
+    public partial class Form1 : Form
+    {
+        string cadenaConexion;
+        public Form1()
+        {
+            InitializeComponent();
+            cadenaConexion = "Initial Catalog=Futbol; Data Source=localhost; user=sa; password=montecastelo";
+            lstEquipos.DataSource = GetEquipos();
+        }
+
+        private void lstEquipos_DoubleClick(object sender, EventArgs e)
+        {
+            Equipo equipoSeleccionado = lstEquipos.SelectedItem as Equipo;
+            lstFutbolistas.DataSource = GetFutbolistasByEquipo(equipoSeleccionado);
+        }
+
+        public List<Equipo> GetEquipos()
+        {
+            using (IDbConnection db = new SqlConnection(cadenaConexion))
+            {
+                var consulta = @"SELECT * FROM Equipos";
+                List<Equipo> listaEquipos = (List<Equipo>)db.Query<Equipo>(consulta);
+                return listaEquipos;
+            }
+        }
+
+        public List<Futbolista> GetFutbolistasByEquipo(Equipo equipo)
+        {
+            using (IDbConnection db = new SqlConnection(cadenaConexion))
+            {
+                var consulta = $@"SELECT F.* FROM Futbolistas F INNER JOIN Equipos E ON f.CodigoEquipo = e.Codigo WHERE E.codigo = '{equipo.Codigo}'";
+                List<Futbolista> listaFutbolistas = (List<Futbolista>)db.Query<Futbolista>(consulta);
+                return listaFutbolistas;
+            }
+        }
+
+    }
+}

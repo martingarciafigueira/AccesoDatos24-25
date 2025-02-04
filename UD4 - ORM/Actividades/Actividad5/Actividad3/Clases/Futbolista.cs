7238c97c34b90ca0b4208c3338db1dc1d773b7b9
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2025-02-04 19:17:20 +0100

    A6

---
+++ UD4 - ORM/Actividades/Actividad5/Actividad3/Clases/Futbolista.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad3.Clases
+{
+    public class Futbolista
+    {
+        private string codigo;
+        private string nombre;
+        private string codigoEquipo;
+        private string posicion;
+        private int edad;
+        private int goles;
+        private int ta;
+        private int tr;
+        private int minutos;
+        private string precioMercado;
+        private int dorsal;
+        private int peso;
+
+        public Futbolista()
+        {
+        }
+
+        public Futbolista(string codigo, string nombre, string codigoEquipo, string posicion, int edad, int goles, int ta, int tr, int minutos, string precioMercado, int dorsal, int peso)
+        {
+            this.Codigo = codigo;
+            this.Nombre = nombre;
+            this.CodigoEquipo = codigoEquipo;
+            this.Posicion = posicion;
+            this.Edad = edad;
+            this.Goles = goles;
+            this.Ta = ta;
+            this.Tr = tr;
+            this.Minutos = minutos;
+            this.PrecioMercado = precioMercado;
+            this.Dorsal = dorsal;
+            this.Peso = peso;
+        }
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string CodigoEquipo { get => codigoEquipo; set => codigoEquipo = value; }
+        public string Posicion { get => posicion; set => posicion = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Goles { get => goles; set => goles = value; }
+        public int Ta { get => ta; set => ta = value; }
+        public int Tr { get => tr; set => tr = value; }
+        public int Minutos { get => minutos; set => minutos = value; }
+        public string PrecioMercado { get => precioMercado; set => precioMercado = value; }
+        public int Dorsal { get => dorsal; set => dorsal = value; }
+        public int Peso { get => peso; set => peso = value; }
+
+        public override string ToString()
+        {
+            return this.Nombre;
+        }
+
+    }
+}
---
+++ UD4 - ORM/Actividades/Actividad6/Actividad3/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Actividad3.Clases;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad3
+{
+    public partial class Form1 : Form
+    {
+        string cadenaConexion;
+        public Form1()
+        {
+            InitializeComponent();
+            cadenaConexion = "Initial Catalog=Futbol; Data Source=localhost; user=sa; password=montecastelo";
+            lstEquipos.DataSource = GetEquipos();
+            cbEquipoSeleccionado.DataSource = GetEquipos();
+        }
+
+        private void lstEquipos_DoubleClick(object sender, EventArgs e)
+        {
+            Equipo equipoSeleccionado = lstEquipos.SelectedItem as Equipo;
+            lstFutbolistas.DataSource = GetFutbolistasByEquipo(equipoSeleccionado);
+        }
+
+        public List<Equipo> GetEquipos()
+        {
+            using (IDbConnection db = new SqlConnection(cadenaConexion))
+            {
+                var consulta = @"SELECT * FROM Equipos";
+                List<Equipo> listaEquipos = (List<Equipo>)db.Query<Equipo>(consulta);
+                return listaEquipos;
+            }
+        }
+
+        public List<Futbolista> GetFutbolistasByEquipo(Equipo equipo)
+        {
+            using (IDbConnection db = new SqlConnection(cadenaConexion))
+            {
+                var consulta = $@"SELECT F.* FROM Futbolistas F INNER JOIN Equipos E ON f.CodigoEquipo = e.Codigo WHERE E.codigo = '{equipo.Codigo}'";
+                List<Futbolista> listaFutbolistas = (List<Futbolista>)db.Query<Futbolista>(consulta);
+                return listaFutbolistas;
+            }
+        }
+
+        private void btnInsertarEquipos_Click(object sender, EventArgs e)
+        {
+            Equipo nuevo = new Equipo();
+            nuevo.Codigo = txtCodigoEquipo.Text;
+            nuevo.Nombre = txtNombreEquipo.Text;
+            nuevo.Estadio = txtEstadio.Text;
+
+            InsertarEquipo(nuevo);
+            lstEquipos.DataSource = GetEquipos();
+            txtCodigoEquipo.Clear();
+            txtNombreEquipo.Clear();
+            txtEstadio.Clear();
+        }
+
+        public void InsertarEquipo(Equipo nuevoEquipo)
+        {
+            var consulta = $@"INSERT INTO Equipos (Codigo, Nombre, Estadio) VALUES (@Codigo, @Nombre, @Estadio)";
+
+            using (IDbConnection db = new SqlConnection(cadenaConexion))
+            {
+                db.Execute(consulta, nuevoEquipo);
+            }
+        }
+
+        private void btnInsertarFutbolista_Click(object sender, EventArgs e)
+        {
+            Futbolista nuevo = new Futbolista();
+            nuevo.Codigo = txtCodigoFutbolista.Text;
+            nuevo.Nombre = txtNombreFutbolista.Text;
+            Equipo equipoSeleccionado = cbEquipoSeleccionado.SelectedValue as Equipo;
+            nuevo.CodigoEquipo = equipoSeleccionado.Codigo;
+
+            InsertarFutbolista(nuevo);
+            txtCodigoFutbolista.Clear();
+            txtNombreFutbolista.Clear();
+            cbEquipoSeleccionado.SelectedIndex = 0;
+        }
+
+        public void InsertarFutbolista(Futbolista nuevoFutbolista)
+        {
+            var consulta = $@"INSERT INTO Futbolistas(Codigo, Nombre, CodigoEquipo) VALUES (@Codigo, @Nombre, @CodigoEquipo)";
+
+            using (IDbConnection db = new SqlConnection(cadenaConexion))
+            {
+                db.Execute(consulta, nuevoFutbolista);
+            }
+        }
+    }
+}
